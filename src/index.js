abf9fb2950e06f0e265ece855ff8da22b7edefbb
Anderson lopes <andersonlopes017.al@gmail.com>
2024-02-14 02:53:40 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicar(numero1, numero2) {
+    return numero1 * numero2;
+}
+const resultadoMultiplicacao = multiplicar(2, 5);
+console.log(`Resultado da multiplicação: ${resultadoMultiplicacao}`);
+function saudar(nome) {
+    return `Olá ${nome}`;
+}
+const saudacao = saudar("João");
+console.log(`Saudação: ${saudacao}`);
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+function multiplicar(numero1: number, numero2: number): number {
+  return numero1 * numero2;
+}
+
+const resultadoMultiplicacao = multiplicar(2, 5);
+
+console.log(`Resultado da multiplicação: ${resultadoMultiplicacao}`);
+
+function saudar(nome: string): string {
+  return `Olá ${nome}`;
+}
+
+const saudacao = saudar("João");
+
+console.log(`Saudação: ${saudacao}`);
